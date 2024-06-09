67790a5108ce94ade41c49432294ffcf106b7dff
Avinash Pandey <avinash.12009084@lpu.in>
2024-06-10 00:18:58 +0530

    Messages API Part

---
+++ src/app/api/accept-message/route.ts
@@ -0,0 +1,111 @@
+import connectMongoDB from "@/lib/connectMongoDB";
+import UserModel from "@/models/User";
+import { authOptions } from "../auth/[...nextauth]/options";
+import { User, getServerSession } from "next-auth";
+
+export async function POST(request: Request) {
+  await connectMongoDB();
+
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user;
+
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not Authenticated",
+      },
+      { status: 401 }
+    );
+  }
+
+  const userId = user._id;
+  const { acceptMessages } = await request.json();
+
+  try {
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      userId,
+      { isAcceptingMessage: acceptMessages },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "Failed to update user status",
+        },
+        { status: 500 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "Message accepted and status updated success",
+        updatedUser,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log("Failed to update user status to accept messages");
+
+    return Response.json(
+      {
+        success: false,
+        message: "Failed to update user status",
+      },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET(request: Request) {
+  await connectMongoDB();
+
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user;
+
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not Authenticated",
+      },
+      { status: 401 }
+    );
+  }
+
+  const userId = user._id;
+
+  try {
+    const foundUser = await UserModel.findById(userId);
+
+    if (!foundUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "User Not Found 404",
+        },
+        { status: 404 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        isAcceptingMessages: foundUser.isAcceptingMessage,
+      },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    console.log("Failed to get the message" + error.message);
+    return Response.json(
+      {
+        success: false,
+        message: "Error in getting Message",
+      },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,55 @@
+import connectMongoDB from "@/lib/connectMongoDB";
+import UserModel, { Message } from "@/models/User";
+
+export async function POST(request: Request) {
+  await connectMongoDB();
+
+  const { username, content } = await request.json();
+
+  try {
+    const user = await UserModel.findOne({ username });
+
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found!",
+        },
+        { status: 404 }
+      );
+    }
+
+    if (!user.isAcceptingMessage) {
+      return Response.json(
+        {
+          success: false,
+          message: "User is not accepting messages",
+        },
+        { status: 403 }
+      );
+    }
+
+    const newMessage = { content, createdAt: new Date() };
+
+    user.messages.push(newMessage as Message);
+
+    await user.save();
+
+    return Response.json(
+      {
+        success: true,
+        message: "Sent Successfully",
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log("Message Sending Server Error in send messages");
+    return Response.json(
+      {
+        success: false,
+        message: "Message Sending Server Error",
+      },
+      { status: 500 }
+    );
+  }
+}
