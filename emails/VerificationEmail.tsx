7a22ce0a3fe704ecd93086385336659f82d777ff
Avinash Pandey <avinash.12009084@lpu.in>
2024-07-17 13:33:21 +0530

    Nodemailer Added

--- emails/VerificationEmail.tsx
+++ emails/VerificationEmail.tsx
@@ -7,6 +7,7 @@ import {
   Row,
   Section,
   Text,
+  Container,
 } from "@react-email/components";
 
 interface VerificationEmailProps {
@@ -21,7 +22,7 @@ export default function VerificationEmail({
   return (
     <Html lang="en" dir="ltr">
       <Head>
-        <title>Verification Code</title>
+        <title className="text-sm font-thin">Verification Code</title>
         <Font
           fontFamily="Roboto"
           fallbackFontFamily="Verdana"
@@ -33,34 +34,46 @@ export default function VerificationEmail({
           fontStyle="normal"
         />
       </Head>
-      <Preview>Here&apos;s your verification code: {otp}</Preview>
-      <Section>
-        <Row>
-          <Heading as="h4">Hello {username},</Heading>
-        </Row>
-        <Row>
-          <Text>
-            Thank you for registering. Please use the following verification
-            code to complete your registration:
+      <Preview>
+        {`Here's`} your verification code: {otp}
+      </Preview>
+      <Container className="bg-gray-100 p-5 font-roboto">
+        <Section className="bg-white p-6 rounded-lg shadow-md">
+          <Row>
+            <Heading as="h1" className="text-gray-900 text-2xl mb-4">
+              Verification Code
+            </Heading>
+          </Row>
+          <Row>
+            <Text className="text-gray-900 text-lg mb-2">
+              Hello {username},
+            </Text>
+          </Row>
+          <Row>
+            <Text className="text-gray-700 text-base mb-4">
+              Thank you for registering. Please use the following verification
+              code to complete your registration:
+            </Text>
+          </Row>
+          <Row>
+            <Text className="text-blue-600 text-xl font-bold mb-4">{otp}</Text>
+          </Row>
+          <Row>
+            <Text className="text-gray-700 text-base">
+              If you did not request this code, please ignore this email.
+            </Text>
+          </Row>
+        </Section>
+        <Section className="bg-gray-100 p-4 text-center border-t mt-6">
+          <Text className="text-gray-700 text-sm">Best regards,</Text>
+          <Text className="text-gray-700 text-sm">Replibot</Text>
+          <Text className="text-gray-700 text-sm">
+            <a href="https://replibot.vercel.app" className="text-orange-600">
+              Visit our website
+            </a>
           </Text>
-        </Row>
-        <Row>
-          <Text>{otp}</Text>
-        </Row>
-        <Row>
-          <Text>
-            If you did not request this code, please ignore this email.
-          </Text>
-        </Row>
-        {/* <Row>
-          <Button
-            href={`http://localhost:3000/verify/${username}`}
-            style={{ color: '#61dafb' }}
-          >
-            Verify here
-          </Button>
-        </Row> */}
-      </Section>
+        </Section>
+      </Container>
     </Html>
   );
 }
--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,6 +1,43 @@
-import { resend } from "@/lib/resend";
+// import { resend } from "@/lib/resend";
+// import VerificationEmail from "../../emails/VerificationEmail";
+// import { ApiResponse } from "@/types/ApiResponse";
+
+// export async function sendVerificationEmail(
+//   email: string,
+//   username: string,
+//   verifyCode: string
+// ): Promise<ApiResponse> {
+//   try {
+//     await resend.emails.send({
+//       from: "replibot.ai <onboarding@resend.dev>",
+//       to: email,
+//       subject: "RepliBot | Verification Code",
+//       react: VerificationEmail({ username, otp: verifyCode }),
+//     });
+
+//     // console.log("Send Mail OTP" + data);
+
+//     return { success: true, message: "Success to send verification email" };
+//   } catch (emailError) {
+//     console.log("Email Failed", emailError);
+//     return { success: false, message: "Failed to send verification email" };
+//   }
+// }
+
+import nodemailer from "nodemailer";
 import VerificationEmail from "../../emails/VerificationEmail";
 import { ApiResponse } from "@/types/ApiResponse";
+import { render } from "@react-email/components";
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
 
 export async function sendVerificationEmail(
   email: string,
@@ -8,14 +45,15 @@ export async function sendVerificationEmail(
   verifyCode: string
 ): Promise<ApiResponse> {
   try {
-    await resend.emails.send({
-      from: "replibot.ai <onboarding@resend.dev>",
-      to: email,
-      subject: "RepliBot | Verification Code",
-      react: VerificationEmail({ username, otp: verifyCode }),
-    });
+    // Render the email content using @react-email/components
+    const emailHtml = render(VerificationEmail({ username, otp: verifyCode }));
 
-    // console.log("Send Mail OTP" + data);
+    await transporter.sendMail({
+      from: '"replibot.ai" <onboarding@replibot.ai>', // sender address
+      to: email, // list of receivers
+      subject: "RepliBot | Verification Code", // Subject line
+      html: emailHtml, // html body
+    });
 
     return { success: true, message: "Success to send verification email" };
   } catch (emailError) {
